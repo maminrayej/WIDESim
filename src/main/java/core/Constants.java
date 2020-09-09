9409c125e5d78980a2d0146e70f9c80533123069
m.amin.rayej <m.amin.rayej@gmail.com>
2020-09-09 10:39:09 +0430

    init project and implement basic topology parser.

---
+++ src/main/java/core/Constants.java
@@ -0,0 +1,38 @@
+package core;
+
+public class Constants {
+
+    private Constants() {
+        throw new UnsupportedOperationException("Can not instantiate class: Constants");
+    }
+
+    public static class DataUnit {
+        public static final long BYTE = 8;
+        public static final long KB = 1024 * BYTE;
+        public static final long MB = 1024 * KB;
+        public static final long GB = 1024 * MB;
+        public static final long TB = 1024 * GB;
+    }
+
+    public static class MetricUnit {
+        public static final long DECA = 10;
+        public static final long HECTO = 10 * DECA;
+        public static final long KILO = 10 * HECTO;
+        public static final long MEGA = 10 * KILO;
+        public static final long GIGA = 10 * MEGA;
+        public static final long TERA = 10 * GIGA;
+    }
+
+    public static class PowOfTwo {
+        public static final int ONE = 2;
+        public static final int TWO = 2 * ONE;
+        public static final int THREE = 2 * TWO;
+        public static final int FOUR = 2 * THREE;
+        public static final int FIVE = 2 * FOUR;
+        public static final int SIX = 2 * FIVE;
+        public static final int SEVEN = 2 * SIX;
+        public static final int EIGHT = 2 * SEVEN;
+        public static final int NINE = 2 * EIGHT;
+        public static final int TEN = 2 * NINE;
+    }
+}
---
+++ src/main/java/parse/TopologyParser.java
@@ -0,0 +1,106 @@
+package parse;
+
+import core.Constants;
+import org.cloudbus.cloudsim.*;
+import org.cloudbus.cloudsim.provisioners.BwProvisionerSimple;
+import org.cloudbus.cloudsim.provisioners.PeProvisionerSimple;
+import org.cloudbus.cloudsim.provisioners.RamProvisionerSimple;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.xml.crypto.Data;
+import java.util.IllegalFormatException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TopologyParser {
+
+    public static void parse(String json) throws JSONException {
+        JSONObject root = new JSONObject(json);
+
+        JSONArray fogDevicesNode = root.getJSONArray("fog_devices");
+
+        List<Datacenter> fogDevices = root.getJSONArray("fog_devices").toList().stream().map(fogDevice -> {
+            JSONObject fogDeviceObj = (JSONObject) fogDevice;
+            String deviceId = fogDeviceObj.getString("device_id");
+            String arch = getOr(fogDeviceObj, "architecture", "x86_64", String.class);
+            String os = getOr(fogDeviceObj, "os", "Linux", String.class);
+            List<String> neighborIds = fogDeviceObj
+                    .getJSONArray("neighbors")
+                    .toList()
+                    .stream()
+                    .map(obj -> ((JSONObject) obj).getString("fog_device_id"))
+                    .collect(Collectors.toList());
+
+            List<Host> hosts = fogDeviceObj.getJSONArray("hosts").toList().stream().map(host -> {
+                JSONObject hostObj = (JSONObject) host;
+                int hostId = hostObj.getInt("host_id");
+                Long storageCap = getOr(hostObj, "storage_cap", Constants.DataUnit.GB, Long.class);
+                int ram = getOr(hostObj, "ram", Constants.PowOfTwo.NINE, int.class);
+                long bw = getOr(hostObj, "bw", (long)Constants.PowOfTwo.TEN, long.class);
+                String ramProvisioning = getOr(hostObj, "ram_provisioning", "Simple", String.class);
+                String bwProvisioning = getOr(hostObj, "bw_provisioning", "Simple", String.class);
+                String vmScheduler = getOr(hostObj, "vm_scheduler", "TimeShared", String.class);
+
+                List<Vm> vms = hostObj.getJSONArray("vms").toList().stream().map(vm -> {
+                    JSONObject vmObj = (JSONObject) vm;
+                    int vmId = vmObj.getInt("vm_id");
+                    Long size = getOr(vmObj, "size", Constants.DataUnit.MB, Long.class);
+                    double mips = getOr(vmObj, "mips", (double)Constants.MetricUnit.GIGA, double.class);
+                    int numOfPes = getOr(vmObj, "num_of_pes", 1, int.class);
+                    int vmRam = getOr(vmObj, "ram", Constants.PowOfTwo.NINE, int.class);
+                    long vmBw = getOr(vmObj, "bw", (long)Constants.PowOfTwo.TEN, long.class);
+                    String vmm = getOr(vmObj, "vmm", "Xen", String.class);
+                    String cloudlet_scheduler = getOr(vmObj, "cloudlet_scheduler", "SpaceShared", String.class);
+
+                    return new Vm(vmId, hostId, mips, numOfPes, vmRam, vmBw, size, vmm, new CloudletSchedulerSpaceShared());
+                }).collect(Collectors.toList());
+
+                List<Pe> pes = hostObj.getJSONArray("pes").toList().stream().map(pe -> {
+                    JSONObject peObj = (JSONObject) pe;
+                    int peId = peObj.getInt("pe_id");
+                    double mips = getOr(peObj, "mips", (double)Constants.PowOfTwo.TEN, double.class);
+                    String peProvisioning = getOr(peObj, "pe_provisioning", "Simple", String.class);
+
+                    return new Pe(peId, new PeProvisionerSimple(mips));
+                }).collect(Collectors.toList());
+
+                return new Host(hostId, new RamProvisionerSimple(ram), new BwProvisionerSimple(bw), storageCap, pes, new VmSchedulerTimeShared(pes));
+            }).collect(Collectors.toList());
+
+            double timeZone = getOr(fogDeviceObj, "time_zone", 0.0, double.class);
+            double costPerSec = getOr(fogDeviceObj, "cost_per_sec", 1.0, double.class);
+            double costPerStorage = getOr(fogDeviceObj, "cost_per_storage", 1.0, double.class);
+            double costPerMemory = getOr(fogDeviceObj, "cost_per_memory", 1.0, double.class);
+            double costPerBw = getOr(fogDeviceObj, "cost_per_bw", 1.0,  double.class);
+            String vmAllocationPolicy = getOr(fogDeviceObj, "vm_allocation_policy", "Simple", String.class);
+            String allocationPolicy = getOr(fogDeviceObj, "allocation_policy", "TimeShared", String.class);
+            String vmm = getOr(fogDeviceObj, "vmm", "Xen", String.class);
+            double schedulingInterval = getOr(fogDeviceObj, "schedule_interval", 0.0, double.class);
+
+            DatacenterCharacteristics characteristics = new DatacenterCharacteristics(arch, os, vmm, hosts, timeZone, costPerSec, costPerMemory, costPerStorage, costPerBw);
+
+            try {
+                return new Datacenter(deviceId, characteristics, new VmAllocationPolicySimple(hosts), new LinkedList<>(), schedulingInterval);
+            } catch (Exception e) {
+                throw new IllegalStateException(e.getMessage());
+            }
+
+        }).collect(Collectors.toList());
+    }
+
+    private static <T> T getOr(JSONObject jsonObject, String key, T defaultValue, Class<T> type) {
+        if (jsonObject.isNull(key)) {
+            return defaultValue;
+        } else {
+            Object obj = jsonObject.get(key);
+            if (obj.getClass() != type)
+                throw new ClassCastException(String.format("Type %s is not the same as %s", type, obj.getClass()));
+
+            return type.cast(obj);
+        }
+    }
+
+}
