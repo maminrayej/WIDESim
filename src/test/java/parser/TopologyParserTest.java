9a54211f198a8960883bcc3366c5bdfb46448df0
m.amin rayej <m.amin.rayej@gmail.com>
2020-09-21 10:31:15 +0330

    prepare test environment

---
+++ src/test/java/parser/TopologyParserTest.java
@@ -0,0 +1,11 @@
+package parser;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TopologyParserTest {
+    @Test
+    void simpleTest() {
+        Assertions.assertTrue(true);
+    }
+}
