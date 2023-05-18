5f25937e4f31ab541d528493c39a54b6734c30ce
Parsa Mohammadian <parsa2820@gmail.com>
2023-05-18 19:30:44 +0330

    Add new examples

--- src/main/java/widesim/examples/dax/CyberShake100.java
+++ src/main/java/widesim/examples/dax/CyberShake100.java
@@ -1,5 +1,8 @@
 package widesim.examples.dax;
 
+import org.cloudbus.cloudsim.Vm;
+import org.cloudbus.cloudsim.core.CloudSim;
+import org.cloudbus.cloudsim.power.PowerVm;
 import widesim.computation.Task;
 import widesim.computation.Workflow;
 import widesim.core.Logger;
@@ -12,16 +15,13 @@ import widesim.parse.dax.DaxParser;
 import widesim.parse.topology.Parser;
 import widesim.parse.topology.PostProcessor;
 import widesim.provision.SimpleVmProvisioner;
-import org.cloudbus.cloudsim.power.PowerVm;
-import org.cloudbus.cloudsim.Vm;
-import org.cloudbus.cloudsim.core.CloudSim;
 
 import java.io.File;
 import java.util.Calendar;
 import java.util.List;
 import java.util.stream.IntStream;
 
-public class Montage25 {
+public class CyberShake100 {
     public static void main(String[] args) throws Exception {
         CloudSim.init(1, Calendar.getInstance(), false);
 
@@ -46,7 +46,7 @@ public class Montage25 {
         });
         fogBroker.submitVmList(vms);
 
-        var daxParser = new DaxParser("src/main/resources/dax/Montage_25.xml");
+        var daxParser = new DaxParser("src/main/resources/dax/CyberShake_100.xml");
         var workflows = List.of(daxParser.buildWorkflow());
 
         for (Workflow workflow: workflows) {
